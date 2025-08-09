540467fcb39b1d312b851259cc644a035cf292f4
yaseen-naseer <34091706+yaseen-naseer@users.noreply.github.com>
2025-08-10 01:28:44 +0500

    Fix scoreboard link base path

--- src/components/SettingsPage.tsx
+++ src/components/SettingsPage.tsx
@@ -89,7 +89,9 @@ export const SettingsPage: React.FC<SettingsPageProps> = ({
 
   const cancelResetGame = () => setShowResetConfirm(false);
 
-  const scoreboardUrl = `${window.location.origin}/scoreboard?embed=true&theme=${theme}`;
+  const { origin, pathname } = window.location;
+  const basePath = pathname.substring(0, pathname.lastIndexOf('/'));
+  const scoreboardUrl = `${origin}${basePath}/scoreboard?embed=true&theme=${theme}`;
 
   return (
     <div className="p-8 max-w-3xl mx-auto">
